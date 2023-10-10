50ffa36f21ee78592fd152b647dbe0487b6f5799
Jeeranunnat <jeeranun.work@gmail.com>
2023-10-11 01:35:41 +0700

    update href in cardsdata

--- src/components/CardComponents/CardWorkshop.js
+++ src/components/CardComponents/CardWorkshop.js
@@ -10,11 +10,13 @@ const Card = (item) => {
       className={theme === 'light' ? 'cardWorkshop-light' : 'cardWorkshop-dark'}
     >
       <div className="card-img">
-        <img src={item.imageUrl} alt={item.name}></img>
+        <img src={item.imagePath} alt={item.name}></img>
       </div>
       <div className="card-name">{item.name}</div>
       <div className="card-link link">
-        <button>Demo</button>
+        <button>
+          <a href={item.href}>Demo</a>
+        </button>
       </div>
     </div>
   );
